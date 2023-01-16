0b6b48b75f3a794082a0cf149e91fbdd81abc131
maxentr <rouaultmaxent@gmail.com>
2023-01-16 19:49:40 +0100

    fix: change csv file path for dev

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-const CSV_FILE_PATH = "./csv";
+const CSV_FILE_PATH = `.${process.env.ENV === "DEV" ? "" : "/dist"}/csv`;
 const CSV_NEW_FILE_PATH = `${CSV_FILE_PATH}/new`;
 const CSV_BAD_INTEGRITY_FILE_PATH = `${CSV_FILE_PATH}/bad-integrity`;
 const CSV_HISTORY_FILE_PATH = `${CSV_FILE_PATH}/history`;
